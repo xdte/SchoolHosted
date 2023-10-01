0f9a4d1391ddf9b968963b9abbee1db6f2548723
Qooui <86065083+xdte@users.noreply.github.com>
2023-10-01 14:22:18 +0800

    Update bus.py #3

--- bus.py
+++ bus.py
@@ -58,5 +58,5 @@ while True:
         with placeholder.container():
             st.write(f"{direction[0]['name']}")
             st.table(df)
-            st.write(f"Last Updated: {str(datetime.now(pytz.timezone("Hongkong")))[:-7]}")
+            st.write(f"Last Updated: {str(datetime.now(pytz.timezone('Hongkong')))[:-7]}")
         time.sleep(8)
