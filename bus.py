4ab17b8ae35e3173b12523930c7b7c8031de1f35
Qooui <86065083+xdte@users.noreply.github.com>
2023-10-13 14:26:55 +0800

    Update bus.py

--- bus.py
+++ bus.py
@@ -18,9 +18,10 @@ stops = [[{"id": "942E95B4336BDFA7", "name": "Uphill"},
          [{"id": "58611212645F0AB1", "name": "Downhill"}],
          [{"id": "6D1FFB57C26F1108", "name": "Choi Wan Bus Terminus"},
          {"id": "09680C5849BFA077", "name": "Choi Wan Bus Terminus"},
-         {"id": "177A516A81E9DEA5", "name": "Choi Wan Bus Terminus"}]]
+         {"id": "177A516A81E9DEA5", "name": "Choi Wan Bus Terminus"}],
+         "MTR"]
 url = "https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/"
-st.set_page_config(page_title="Bus ETA Information")
+st.set_page_config(page_title="ETA Information")
 placeholder = st.empty()
 container = st.container()
 hide_st_style = """
@@ -35,43 +36,59 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 while True:
     for direction in stops:
         formatted = {}
-        for stop in direction:
-            response = requests.get(url+stop['id']) 
-            if response.status_code == 200:
-                data = response.json()['data']
-                for i in data:
-                    if i['eta'] != None and i["dest_en"] != "CHOI WAN" and i['eta_seq'] <= 3:
-                        try:
-                            tmp = formatted[str(i['route'])]
-                            del tmp
-                        except KeyError:
-                            formatted[str(i['route'])] = {}
-                            formatted[str(i['route'])]["Destination"] = i["dest_en"]
-                            formatted[str(i["route"])]["ETA"] = []
-                        formatted[str(i['route'])]["ETA"].append(i["eta"])         
-            else:
-                print("Request failed with status code:", response.status_code)
-        for i in formatted:
-            formatted[i]['ETA'] = sorted(list(set(formatted[i]['ETA'])))
-            s = []
-            for j in formatted[i]["ETA"]:
-                diff = calctimediff(j, datetime.now(pytz.timezone("Hongkong")).isoformat())
-                mins = diff//60
-                secs = diff%60
-                if mins == 0:
-                    s.append("<1")
+        if direction == "MTR":
+            response = requests.get("https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=KTL&sta=CHH").json()["data"]["KTL-CHH"]
+            formatted["MTR to Tiu Keng Leng"] = {}
+            formatted["MTR to Tiu Keng Leng"]["ETA"] = []
+            formatted["MTR to Whampoa/Ho Man Tin"] = {}
+            formatted["MTR to Whampoa/Ho Man Tin"]["ETA"] = []
+            for i in response["UP"]:
+                formatted["MTR to Tiu Keng Leng"]["ETA"].append(i["ttnt"])
+            formatted["MTR to Tiu Keng Leng"]["ETA"] = " | ".join(formatted["MTR to Tiu Keng Leng"]["ETA"])
+            for i in response["DOWN"]:
+                formatted["MTR to Whampoa/Ho Man Tin"]["ETA"].append(i["ttnt"])
+            formatted["MTR to Whampoa/Ho Man Tin"]["ETA"] = " | ".join(formatted["MTR to Whampoa/Ho Man Tin"]["ETA"])
+        else:
+            for stop in direction:
+                response = requests.get(url+stop['id']) 
+                if response.status_code == 200:
+                    data = response.json()['data']
+                    for i in data:
+                        if i['eta'] != None and i["dest_en"] != "CHOI WAN" and i['eta_seq'] <= 3:
+                            try:
+                                tmp = formatted[str(i['route'])]
+                                del tmp
+                            except KeyError:
+                                formatted[str(i['route'])] = {}
+                                formatted[str(i['route'])]["Destination"] = i["dest_en"]
+                                formatted[str(i["route"])]["ETA"] = []
+                            formatted[str(i['route'])]["ETA"].append(i["eta"])         
                 else:
-                    if secs > 30:
-                        mins+=1
-                    s.append(f"{mins}")
-            formatted[i]['ETA'] = '     | '.join(s)
-            del s
+                    print("Request failed with status code:", response.status_code)
+            for i in formatted:
+                formatted[i]['ETA'] = sorted(list(set(formatted[i]['ETA'])))
+                s = []
+                for j in formatted[i]["ETA"]:
+                    diff = calctimediff(j, datetime.now(pytz.timezone("Hongkong")).isoformat())
+                    mins = diff//60
+                    secs = diff%60
+                    if mins == 0:
+                        s.append("<1")
+                    else:
+                        if secs > 30:
+                            mins+=1
+                        s.append(f"{mins}")
+                formatted[i]['ETA'] = '  |  '.join(s)
+                del s
 
         df = pd.DataFrame(formatted).transpose()
         with placeholder.container():
-            st.header('Bus ETA Information')
-            st.write(f"{direction[0]['name']} {stops.index(direction)+1}/{len(stops)}")
+            st.write('ETA Information')
+            if direction != "MTR":
+                st.write(f"{direction[0]['name']} {stops.index(direction)+1}/{len(stops)}")
+            else:
+                st.write('Choi Hung MTR')
             st.table(df)
             st.write(f"Last Updated: {str(datetime.now(pytz.timezone('Hongkong')))[:-13]}")
-            st.caption("Made with ❤ by Bonfire")
-        time.sleep(5.5)
+            st.caption("v1.2.5 Made with ❤ by Bonfire")
+        time.sleep(6 if direction!="MTR" else 4)
